4020015917556310a3a34da24a92342109203711
Khevana_Patel <Khevana.patel@quifers.com>
2019-11-30 13:33:11 +0530

    applying strong password validation

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -133,6 +133,9 @@ return [
         'attribute-name' => [
             'rule-name' => 'custom-message',
         ],
+        'password' => [
+            'regex' => 'Password must contain letters, numbers and special character(!@#$%)',
+        ],
     ],
 
     /*
